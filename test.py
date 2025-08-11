8f9749041ee5a5482aa49799da425f11aa005808
yuu0930 <rqsnq531@gmail.com>
2025-08-11 21:54:21 +0900

    test commit

---
+++ test.py
@@ -0,0 +1,45 @@
+print("Hello!　こんにちは！")
+print("Hey!" + "brother")
+print(len("Hello World"))
+print(24 * 365)
+print("i am" + " " + str(20) + " " + "years old")
+print(int(100) + 400)
+print(f"今日で{19 + 1}歳になりました")
+print(f"タロウさん、{5}分は{5 * 60}秒です")
+
+name = "タロウ"
+print(name)
+
+pi = 3.14159
+print(pi)
+
+value = "Hello World"
+print(value)
+
+value = "See you"
+print(value)
+
+number = 7
+number = number + 1
+print(number)
+
+number += 1
+print(number)
+
+name = "タロウ"
+
+minutes = 5
+seconds = minutes * 60
+
+print(f"{name}さん、{minutes}は{seconds}秒です")
+
+name = input()
+print(f"{name}さん")
+
+text = input()
+print(f"入力したのは{text}です")
+
+text = input()
+print(f"入力したのは{text}です")
+print(f"入力したのは{text}です\n")
+print(f"入力したのは{text}です")
