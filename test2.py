881c5a62a312e8a06c4519e5560b36020d5eac94
yuu0930 <rixxn.haruka.loooove@gmail.com>
2025-08-11 22:07:53 +0900

    test commit2

---
+++ test2.py
@@ -0,0 +1,15 @@
+pencil_case = ["ペン", "消しゴム", "ものさし"]
+print(pencil_case)
+
+#配列に情報を追加(配列.append(追加する要素))
+pencil_case.append("えんぴつ")
+print(pencil_case)
+
+print(pencil_case[0])
+
+pencil_case[1] = "修正ペン"
+print(pencil_case)
+
+pencil_case.append("筆箱")
+print(pencil_case)
+print(len(pencil_case))
